c43b41d199e7f14ab0f11539ce7ceae8bb3d2faa
Your Name <you@example.com>
2024-06-03 04:16:59 -0700

    shohidul

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -51,7 +51,6 @@ export default function RegisterPage() {
             if (response.ok) {
                 toast.success("Registration Successful");
                 setTimeout(() => {
-                    window.location.reload()
                     router.push("/login");
                 }, 1000);
             } else {
