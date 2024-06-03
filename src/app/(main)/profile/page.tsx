70b683cac9f130bb0650f87968b4acac880b3a91
Your Name <you@example.com>
2024-06-03 04:04:17 -0700

    frist-commit

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -12,44 +12,39 @@ const ProfilePage = () => {
     const { data: session, status } = useSession();
     const router = useRouter();
 
-    // Fetch all users when the status is authenticated
-    useEffect(() => {
-        if (status === 'authenticated') {
-            const fetchUserData = async () => {
-                try {
-                    const response = await fetch('/api/user', {
-                        cache: 'no-store'
-                    });
-
-                    if (!response.ok) {
-                        throw new Error(`Error fetching user data: ${response.statusText}`);
-                    }
-                    const data: User[] = await response.json();
-                    setAllUsers(data);
-                } catch (err) {
-                    console.error(err);
-                }
-            };
-
-            fetchUserData();
-        }
-    }, [status]);
-
-    // Redirect to login if the user is not authenticated
     useEffect(() => {
         if (status === 'loading') {
             return;
-        } else if (status === "unauthenticated") {
+        } else if (status === 'unauthenticated') {
             router.push('/login');
         }
     }, [status, router]);
 
-    // Filter user data based on the session email
-    const userData = allUsers.find(user => user.email === session?.user?.email);
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch('/api/user', {
+                    cache: 'no-store'
+                });
 
-    // Fetch profile data based on the filtered user data
+                if (!response.ok) {
+                    throw new Error(`Error fetching user data: ${response.statusText}`);
+                }
+                const data: User[] = await response.json();
+                setAllUsers(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        if (status === 'authenticated') {
+            fetchUserData();
+        }
+    }, [status]);
+
     useEffect(() => {
         const fetchProfile = async () => {
+            const userData = allUsers.find(user => user.email === session?.user?.email);
             if (userData?._id) {
                 try {
                     const data = await fetchUserProfile(userData._id);
@@ -62,11 +57,14 @@ const ProfilePage = () => {
             }
         };
 
-        if (userData) {
+        if (allUsers.length > 0 && session?.user) {
             fetchProfile();
         }
-    }, [userData]);
+    }, [allUsers, session]);
 
+    if (status === "loading") {
+        return <div className="flex justify-center items-center h-screen"><p>Loading...</p></div>;
+    }
 
     return (
         <div>
